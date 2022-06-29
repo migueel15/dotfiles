26af2e18e8f21d8641a4de1acf70cfb305d6029c
migueel15 <miguelangeldorado10@gmail.com>
2022-06-29 05:00:01 +0200

    Update Dotfiles - mié 29 jun 2022 05:00:01 CEST

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -32,8 +32,8 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
-
+(setq doom-theme 'doom-palenight)
+(setq doom-font (font-spec :size 15))
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
 (setq display-line-numbers-type t)
