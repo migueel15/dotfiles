24e79afb9d8f90066e4980cb48e0d892f6d7c348
migueel15 <miguelangeldorado10@gmail.com>
2022-07-02 16:44:32 +0200

    Add doom emacs config files

---
+++ .doom.d/config.el
@@ -0,0 +1,15 @@
+(setq user-full-name "Miguel Dorado"
+      user-mail-address "miguelangeldorado10@gmail.com")
+
+(setq doom-theme 'doom-one)
+
+(use-package! org-auto-tangle
+  :defer t
+  :hook (org-mode . org-auto-tangle-mode)
+  :config
+  (setq org-auto-tangle-default t)
+)
+
+(setq display-line-numbers-type t)
+
+(setq org-directory "~/org/")
