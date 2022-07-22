1ed59b24b75454725148be6efc9c6759f7159506
migueel15 <miguelangeldorado10@gmail.com>
2022-07-22 05:21:01 +0200

    Update Dotfiles - vie 22 jul 2022 05:21:01 CEST

--- Scripts/SyncDotfiles.sh
+++ Scripts/SyncDotfiles.sh
@@ -1,6 +1,6 @@
 MESSAGE="Update Dotfiles - $(date)"
 GIT="`which git` --git-dir=/home/firez/.dotfiles/ --work-tree=/home/firez"
-cd ${REPO_DIR}
+# cd ${REPO_DIR}
 ${GIT} add -u 
 ${GIT} commit -m "${MESSAGE}"
 ${GIT} push
