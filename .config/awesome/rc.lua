5966109e6bc6178e037bc26fcb6a985446229de4
migueel15 <miguelangeldorado10@gmail.com>
2022-07-22 05:24:48 +0200

    Update Dotfiles - vie 22 jul 2022 05:24:48 CEST

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -123,7 +123,7 @@ mytextclock = wibox.widget.textclock()
 
 screen.connect_signal("request::desktop_decoration", function(s)
     -- Each screen has its own tag table.
-    awful.tag({ "⚫️", "2", "3", "4", "5" }, s, awful.layout.layouts[1])
+    awful.tag({ "1", "2", "3", "4", "5" }, s, awful.layout.layouts[1])
 
     -- Create a promptbox for each screen
     s.mypromptbox = awful.widget.prompt()
