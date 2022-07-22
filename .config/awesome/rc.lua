4c3719a1d8a717a19827fd86d372328c00fe930e
migueel15 <miguelangeldorado10@gmail.com>
2022-07-22 05:26:30 +0200

    Update Dotfiles - vie 22 jul 2022 05:26:30 CEST

--- .config/awesome/rc.lua
+++ .config/awesome/rc.lua
@@ -162,20 +162,6 @@ screen.connect_signal("request::desktop_decoration", function(s)
         }
     }
 
-    -- Create a tasklist widget
-    s.mytasklist = awful.widget.tasklist {
-        screen  = s,
-        filter  = awful.widget.tasklist.filter.currenttags,
-        buttons = {
-            awful.button({ }, 1, function (c)
-                c:activate { context = "tasklist", action = "toggle_minimization" }
-            end),
-            awful.button({ }, 3, function() awful.menu.client_list { theme = { width = 250 } } end),
-            awful.button({ }, 4, function() awful.client.focus.byidx(-1) end),
-            awful.button({ }, 5, function() awful.client.focus.byidx( 1) end),
-        }
-    }
-
     -- Create the wibox
     s.mywibox = awful.wibar {
         position = "top",
@@ -192,7 +178,6 @@ screen.connect_signal("request::desktop_decoration", function(s)
             nil,
             { -- Right widgets
                 layout = wibox.layout.fixed.horizontal,
-                mykeyboardlayout,
                 wibox.widget.systray(),
                 mytextclock,
                 s.mylayoutbox,
