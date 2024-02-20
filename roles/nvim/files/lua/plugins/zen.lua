f579b8b06dfbe10d359bde238718a9ddb5dfb8dc
migueel15 <miguelangeldorado10@gmail.com>
2024-02-20 09:11:52 +0100

    add: zen plugin

--- roles/nvim/files/lua/plugins/zen.lua
+++ roles/nvim/files/lua/plugins/zen.lua
@@ -1,1 +1,26 @@
-vim.keymap.set("n", "<leader>zz", ":ZenMode<CR>", { noremap = true, silent = true, desc = "ZenMode: Toggle Zen Mode" })
+vim.keymap.set("n", "<leader>zz", function()
+    require("zen-mode").setup {
+        window = {
+            width = 90,
+            options = { }
+        },
+    }
+    require("zen-mode").toggle()
+    vim.wo.wrap = false
+    vim.wo.number = true
+    vim.wo.rnu = true
+end)
+
+vim.keymap.set("n", "<leader>zZ", function()
+    require("zen-mode").setup {
+        window = {
+            width = 80,
+            options = { }
+        },
+    }
+    require("zen-mode").toggle()
+    vim.wo.wrap = false
+    vim.wo.number = false
+    vim.wo.rnu = false
+    vim.opt.colorcolumn = "0"
+end)
