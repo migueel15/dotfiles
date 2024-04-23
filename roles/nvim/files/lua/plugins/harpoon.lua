e564cc9b33d963da6d94fbcf34e0f791b72d5230
migueel15 <miguelangeldorado10@gmail.com>
2024-04-23 02:09:07 +0200

    remove unused keymaps

--- roles/nvim/files/lua/plugins/harpoon.lua
+++ roles/nvim/files/lua/plugins/harpoon.lua
@@ -10,8 +10,3 @@ vim.keymap.set("n", "<Tab>", function()
 	local idx = vim.fn.line(".")
 	harpoon:list():select(idx)
 end)
-
-vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-vim.keymap.set("n", "<C-j>", function() harpoon:list():select(2) end)
-vim.keymap.set("n", "<C-k>", function() harpoon:list():select(3) end)
-vim.keymap.set("n", "<C-l>", function() harpoon:list():select(4) end)
