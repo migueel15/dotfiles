ab52a9ec27fc902a17da588e9d95cb8b9a5b6c31
migueel15 <miguelangeldorado10@gmail.com>
2024-04-23 04:36:01 +0200

    auto update nvim-tree git status

--- roles/nvim/files/lua/plugins/nvim-tree.lua
+++ roles/nvim/files/lua/plugins/nvim-tree.lua
@@ -10,7 +10,7 @@ require("nvim-tree").setup({
 	sync_root_with_cwd = true,
 	update_focused_file = {
 		enable = true,
-		update_root = false,
+		update_root = true,
 	},
 	view = {
 		adaptive_size = true,
