7c0b8e957c6b1da98a5506e6e97a3af6ff76a0eb
migueel15 <miguelangeldorado10@gmail.com>
2024-05-01 06:42:16 +0200

    Update dotfiles: mié 01 may 2024 06:42:16 CEST

--- roles/nvim/files/lua/plugins/harpoon.lua
+++ roles/nvim/files/lua/plugins/harpoon.lua
@@ -6,8 +6,11 @@ return {
 		local harpoon = require("harpoon")
 		harpoon:setup()
 
-		vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+		vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 		vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+		vim.keymap.set("n", "<C-v>", function()
+			-- vsplit selected lines
+		end)
 		vim.keymap.set("n", "<Tab>", function()
 			local idx = vim.fn.line(".")
 			harpoon:list():select(idx)
