dfcb26659de090f7ed362fd4cff0edb9e8c171e8
migueel15 <miguelangeldorado10@gmail.com>
2024-08-31 16:37:10 +0200

    add: new copilot implementation in lua

--- config/files/nvim/lua/plugins/lualine.lua
+++ config/files/nvim/lua/plugins/lualine.lua
@@ -1,21 +1,21 @@
 return {
-	'nvim-lualine/lualine.nvim',
+	"nvim-lualine/lualine.nvim",
 	config = function()
 		require("lualine").setup({
 			options = {
 				icons_enabled = true,
-				theme = 'catppuccin',
-				component_separators = '',
-				section_separators = '',
+				theme = "catppuccin",
+				component_separators = "",
+				section_separators = "",
 			},
 			sections = {
-				lualine_a = { 'mode' },
-				lualine_b = { 'branch', 'diff', 'diagnostics' },
+				lualine_a = { "mode" },
+				lualine_b = { "branch", "diff", "diagnostics" },
 				lualine_c = { "%=", { "filename", path = 1 } },
-				lualine_x = { 'filetype', },
+				lualine_x = { "copilot", "filetype" },
 				lualine_y = { "progress" },
-				lualine_z = { 'location' }
+				lualine_z = { "location" },
 			},
 		})
-	end
+	end,
 }
