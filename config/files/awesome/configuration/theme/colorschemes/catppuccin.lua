171cb3e47fc161ed5357993486f2da78ff7e7fb2
migueel15 <miguelangeldorado10@gmail.com>
2024-12-31 12:01:59 +0100

    add new theme

--- config/files/awesome/configuration/theme/colorschemes/catppuccin.lua
+++ config/files/awesome/configuration/theme/colorschemes/catppuccin.lua
@@ -1,24 +1,24 @@
-local theme      = {}
-theme.font       = "Iosevka Nerd Font Propo, Medium 11"
-theme.wallpaper  = "~/.config/awesome/configuration/theme/wallpapers/catppuccin_landscape.png"
-theme.vscode     = "Catppuccin Mocha"
-theme.obsidian   = "Catppuccin"
-local colors     = {}
+local theme = {}
+theme.font = "Iosevka Nerd Font Propo, Medium 11"
+theme.wallpaper = "~/.config/awesome/configuration/theme/wallpapers/mars-car.jpg"
+theme.vscode = "Catppuccin Mocha"
+theme.obsidian = "Catppuccin"
+local colors = {}
 
-colors.green     = "#a6e3a1"
-colors.cyan      = "#74c7ec"
-colors.blue      = "#89b4fa"
-colors.magenta   = "#cba6f7"
-colors.yellow    = "#f9e2af"
-colors.orange    = "#fab387"
-colors.red       = "#f38ba8"
-colors.white     = "#cdd6f4"
-colors.grey      = "#7f849c"
-colors.darkGrey  = "#45475a"
-colors.dark      = "#1e1e2e"
+colors.green = "#a6e3a1"
+colors.cyan = "#74c7ec"
+colors.blue = "#89b4fa"
+colors.magenta = "#cba6f7"
+colors.yellow = "#f9e2af"
+colors.orange = "#fab387"
+colors.red = "#f38ba8"
+colors.white = "#cdd6f4"
+colors.grey = "#7f849c"
+colors.darkGrey = "#45475a"
+colors.dark = "#1e1e2e"
 
-colors.primary   = colors.dark
+colors.primary = colors.dark
 colors.secondary = colors.blue
-colors.text      = colors.white
-theme.colors     = colors
+colors.text = colors.white
+theme.colors = colors
 return theme
