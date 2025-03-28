1309d64e50349b8439761b615019f3aecde0a714
migueel15 <miguelangeldorado10@gmail.com>
2025-03-28 19:10:46 +0100

    add notion widget sync tasks

--- config/files/awesome/configuration/ui/bar/components/notion_sync.lua
+++ config/files/awesome/configuration/ui/bar/components/notion_sync.lua
@@ -3,65 +3,91 @@ local wibox = require("wibox")
 local naughty = require("naughty")
 local beautiful = require("beautiful")
 local gears = require("gears")
+local json = require("dkjson")
 
 local notion_icon = wibox.widget({
-	widget = wibox.widget.imagebox,
-	resize = true,
-	forced_height = 22,
-	forced_width = 22,
-	image = beautiful.notion_icon,
+  widget = wibox.widget.imagebox,
+  resize = true,
+  forced_height = 22,
+  forced_width = 22,
+  image = beautiful.notion_icon,
 })
 
 function trim(s)
-	return s:match("^%s*(.-)%s*$")
+  return s:match("^%s*(.-)%s*$")
 end
 
 function GetNotionSyncStatus()
-	awful.spawn.easy_async_with_shell("docker inspect notion-sync-cv | jq -r  '.[0].State.Status'", function(stdout)
-		if trim(stdout) == "running" then
-			notion_icon.image = gears.color.recolor_image(beautiful.notion_icon, beautiful.notion_status_color.active)
-		else
-			notion_icon.image = gears.color.recolor_image(beautiful.notion_icon, beautiful.notion_status_color.inactive)
-		end
-	end)
+  awful.spawn.easy_async_with_shell("docker inspect notion-sync-cv | jq -r  '.[0].State.Status'", function(stdout)
+    if trim(stdout) == "running" then
+      notion_icon.image = gears.color.recolor_image(beautiful.notion_icon, beautiful.notion_status_color.active)
+    else
+      notion_icon.image = gears.color.recolor_image(beautiful.notion_icon, beautiful.notion_status_color.inactive)
+    end
+  end)
 end
 
 function ActivateNotionSyncContainer()
-	awful.spawn.easy_async("docker start notion-sync-cv", function(stdout)
-		GetNotionSyncStatus()
-	end)
+  awful.spawn.easy_async("docker start notion-sync-cv", function(stdout)
+    GetNotionSyncStatus()
+  end)
 end
 
 GetNotionSyncStatus()
 
 local notion_widget = wibox.widget({
-	{
-		notion_icon,
-		widget = wibox.container.place,
-		valign = "center",
-		halign = "center",
-	},
-	widget = wibox.container.margin,
-	left = beautiful.widget_margin,
+  {
+    notion_icon,
+    widget = wibox.container.place,
+    valign = "center",
+    halign = "center",
+  },
+  widget = wibox.container.margin,
+  left = beautiful.widget_margin,
 })
 
 notion_widget:connect_signal("button::press", function(_, _, _, button)
-	if button == 1 then
-		ActivateNotionSyncContainer()
-	end
+  if button == 3 then
+    ActivateNotionSyncContainer()
+  end
 end)
 
 gears.timer({
-	timeout = 60,
-	autostart = true,
-	call_now = true,
-	callback = function()
-		GetNotionSyncStatus()
-	end,
+  timeout = 60,
+  autostart = true,
+  call_now = true,
+  callback = function()
+    GetNotionSyncStatus()
+  end,
 })
 
 awesome.connect_signal("notion:request_status", function()
-	GetNotionSyncStatus()
+  GetNotionSyncStatus()
 end)
+
+function getCVTasks()
+  awful.spawn.easy_async_with_shell("cat " .. os.getenv("HOME") .. "/.bw_session", function(bw_session)
+    awful.spawn.easy_async_with_shell(
+      "export BW_SESSION="
+      .. bw_session
+      .. " bash "
+      .. os.getenv("HOME")
+      .. "/.config/awesome/configuration/ui/bar/scripts/notion",
+      function(stdout, stderr, reason, exit_code)
+        if exit_code ~= 0 then
+          naughty.notify({ text = stderr })
+          return
+        end
+        local notion_data = json.decode(stdout)
+        awful.spawn.easy_async_with_shell("echo '" .. stdout .. "' > /tmp/notion_data.json", function()
+          -- send signal to notion widget
+          awesome.emit_signal("notion:tasks", notion_data)
+        end)
+      end
+    )
+  end)
+end
+
+getCVTasks()
 
 return notion_widget
