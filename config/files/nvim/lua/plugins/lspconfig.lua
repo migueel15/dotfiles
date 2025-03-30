39a186fec369f80c86648be8112979fdc4c2b18f
migueel15 <miguelangeldorado10@gmail.com>
2025-03-30 04:36:11 +0200

    add icons to diagnostics

--- config/files/nvim/lua/plugins/lspconfig.lua
+++ config/files/nvim/lua/plugins/lspconfig.lua
@@ -179,5 +179,13 @@ return {
         })
       end,
     })
+
+    -- Setup dianostics
+    vim.diagnostic.config({ virtual_text = true, signs = true })
+    local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
+    for type, icon in pairs(signs) do
+      local hl = "DiagnosticSign" .. type
+      vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+    end
   end,
 }
