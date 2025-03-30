2f3b474be43b29d21a2779a2fb60885dbc8ea50d
migueel15 <miguelangeldorado10@gmail.com>
2025-03-30 04:36:27 +0200

    remove temporary copilot lualine. Wait for fix

--- config/files/nvim/lua/plugins/lualine.lua
+++ config/files/nvim/lua/plugins/lualine.lua
@@ -1,22 +1,23 @@
 return {
-	"nvim-lualine/lualine.nvim",
-	config = function()
-		require("lualine").setup({
-			options = {
-				icons_enabled = true,
-				theme = "catppuccin",
-				component_separators = "",
-				section_separators = "",
-			},
-			sections = {
-				lualine_a = { "mode" },
-				lualine_b = { "branch", "diff", "diagnostics" },
-				lualine_c = { "%=", { "filename", path = 1 } },
-				-- lualine_x = { "filetype" },
-				lualine_x = { "copilot", "filetype" },
-				lualine_y = { "progress" },
-				lualine_z = { "location" },
-			},
-		})
-	end,
+  "nvim-lualine/lualine.nvim",
+  dependencies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    require("lualine").setup({
+      options = {
+        icons_enabled = true,
+        theme = "catppuccin",
+        component_separators = "",
+        section_separators = "",
+      },
+      sections = {
+        lualine_a = { "mode" },
+        lualine_b = { "branch", "diff", "diagnostics" },
+        lualine_c = { "%=", { "filename", path = 1 } },
+        lualine_x = { "filetype" },
+        -- lualine_x = { "copilot", "filetype" },
+        lualine_y = { "progress" },
+        lualine_z = { "location" },
+      },
+    })
+  end,
 }
