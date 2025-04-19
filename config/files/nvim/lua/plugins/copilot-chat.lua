dd05f8318430fd2416dc1ecf6ac194f6ce3188e7
migueel15 <miguelangeldorado10@gmail.com>
2025-04-19 20:27:37 +0200

    add copilot chat and nvim tree new highlight

---
+++ config/files/nvim/lua/plugins/copilot-chat.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "zbirenbaum/copilot.lua" },
+      { "nvim-lua/plenary.nvim", branch = "master" }, -- for curl, log and async functions
+    },
+    build = "make tiktoken",
+    opts = {},
+  },
+}
