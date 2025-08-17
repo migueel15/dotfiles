3fd9ffccdfe58755e57b6a94a38e27ff061364cb
migueel15 <miguelangeldorado10@gmail.com>
2025-08-17 18:21:42 +0200

    update nvim

--- modules/nvim/config/lua/plugins/lspconfig.lua
+++ modules/nvim/config/lua/plugins/lspconfig.lua
@@ -170,16 +170,16 @@ return {
       ensure_installed = vim.tbl_keys(servers),
     })
 
-    mason_lspconfig.setup_handlers({
-      function(server_name)
-        require("lspconfig")[server_name].setup({
-          capabilities = capabilities,
-          on_attach = on_attach,
-          settings = servers[server_name],
-          filetypes = (servers[server_name] or {}).filetypes,
-        })
-      end,
-    })
+    -- mason_lspconfig.setup_handlers({
+    --   function(server_name)
+    --     require("lspconfig")[server_name].setup({
+    --       capabilities = capabilities,
+    --       on_attach = on_attach,
+    --       settings = servers[server_name],
+    --       filetypes = (servers[server_name] or {}).filetypes,
+    --     })
+    --   end,
+    -- })
 
     -- Setup dianostics
     vim.diagnostic.config({ virtual_text = true, signs = true })
