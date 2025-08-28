eab46ee186fe8b2d297852d07f6c2891608c8dcb
migueel15 <miguelangeldorado10@gmail.com>
2025-08-28 14:40:11 +0200

    test

--- modules/nvim/config/lua/core/autocmds.lua
+++ modules/nvim/config/lua/core/autocmds.lua
@@ -1,26 +1,26 @@
 -- highlight on yank
 local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
 vim.api.nvim_create_autocmd("TextYankPost", {
-  callback = function()
-    vim.hl.on_yank()
-  end,
-  group = highlight_group,
-  pattern = "*",
+	callback = function()
+		vim.hl.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
 })
 
 -- format on save
 local formatting_group = vim.api.nvim_create_augroup("LspFormatting", { clear = true })
 
 vim.api.nvim_create_autocmd("BufWritePre", {
-  callback = function()
-    if vim.bo.filetype == "cs" then
-      vim.api.nvim_command("silent! Neoformat")
-    else
-      vim.lsp.buf.format()
-    end
-  end,
-  group = formatting_group,
-  pattern = "*",
+	callback = function()
+		if vim.bo.filetype == "cs" then
+			vim.api.nvim_command("silent! Neoformat")
+		else
+			vim.lsp.buf.format()
+		end
+	end,
+	group = formatting_group,
+	pattern = "*",
 })
 
 vim.api.nvim_command("autocmd User FugitiveChanged NvimTreeRefresh")
@@ -28,18 +28,18 @@ vim.api.nvim_command("autocmd User FugitiveChanged NvimTreeRefresh")
 -- load blanket on java files with jacoco.xml
 local last_modified_time = nil
 vim.api.nvim_create_autocmd("BufEnter", {
-  callback = function()
-    if vim.bo.filetype == "java" then
-      local path = vim.fn.getcwd() .. "/target/site/jacoco/jacoco.xml"
-      if vim.fn.filereadable(path) == 1 then
-        local current_modified_time = vim.fn.getftime(path)
-        if last_modified_time ~= current_modified_time then
-          last_modified_time = current_modified_time
-          vim.cmd("CoverageRefresh")
-        end
-      end
-    end
-  end,
-  group = formatting_group,
-  pattern = "*",
+	callback = function()
+		if vim.bo.filetype == "java" then
+			local path = vim.fn.getcwd() .. "/target/site/jacoco/jacoco.xml"
+			if vim.fn.filereadable(path) == 1 then
+				local current_modified_time = vim.fn.getftime(path)
+				if last_modified_time ~= current_modified_time then
+					last_modified_time = current_modified_time
+					vim.cmd("CoverageRefresh")
+				end
+			end
+		end
+	end,
+	group = formatting_group,
+	pattern = "*",
 })
--- modules/nvim/config/lua/plugins/lspconfig.lua
+++ modules/nvim/config/lua/plugins/lspconfig.lua
@@ -1,84 +1,31 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = { "BufReadPre", "BufNewFile" },
-	config = function()
-		-- Auto-configurar todos los servers instalados por Mason
-		local mason_lspconfig = require("mason-lspconfig")
-
-		for _, server in ipairs(mason_lspconfig.get_installed_servers()) do
-			if server == "lua_ls" then
-				vim.lsp.config('lua_ls', {
-					root_markers = { { '.luarc.json', '.luarc.jsonc' }, '.git' },
-					settings = {
-						Lua = {
-							diagnostics = {
-								globals = { "vim" },
-							},
-							workspace = {
-								library = {
-									[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-									[vim.fn.stdpath("config") .. "/lua"] = true,
-								},
-							},
-						},
-					},
-				})
-			else
-				vim.lsp.config(server, {
-					root_markers = { '.git' },
-				})
-			end
-			vim.lsp.enable(server)
-		end
-
-		-- Configurar keymaps y autocomandos con LspAttach
-		vim.api.nvim_create_autocmd('LspAttach', {
-			callback = function(args)
-				local client = vim.lsp.get_client_by_id(args.data.client_id)
-				local opts = { buffer = args.buf, noremap = true, silent = true }
-
-				-- LSP keybinds usando las nuevas funciones por defecto
-				vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
-				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-				vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
-				vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
-				vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
-				vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
-				vim.keymap.set("n", "<leader>D", vim.diagnostic.open_float, opts)
-				vim.keymap.set("n", "<leader>d", vim.diagnostic.open_float, opts)
-				vim.keymap.set("n", "[d", vim.diagnostic.jump({ count = -1, float = true }), opts)
-				vim.keymap.set("n", "]d", vim.diagnostic.jump({ count = 1, float = true }), opts)
-				vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
-
-
-
-				-- Auto-format al guardar si el server lo soporta
-				if client then
-					if client:supports_method('textDocument/formatting') then
-						vim.api.nvim_create_autocmd('BufWritePre', {
-							buffer = args.buf,
-							callback = function()
-								vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
-							end,
-						})
-					end
-				end
-			end,
-		})
-
-
-		-- Configurar diagnostic signs
-		vim.diagnostic.config({
-			virtual_text = true,
-			signs = {
-				text = {
-					[vim.diagnostic.severity.ERROR] = "",
-					[vim.diagnostic.severity.WARN] = "",
-					[vim.diagnostic.severity.HINT] = "󰠠",
-					[vim.diagnostic.severity.INFO] = "",
-				}
+	{
+		"neovim/nvim-lspconfig",
+	},
+	{
+		"mason-org/mason.nvim",
+		opts = {}
+	},
+	{
+		"mason-org/mason-lspconfig.nvim",
+		dependencies = {
+			"neovim/nvim-lspconfig",
+			"mason-org/mason.nvim"
+		},
+		opts = {
+			ensure_installed = {
+				"lua_ls",
+				"pyright",
+				"ts_ls",
+				"html",
+				"cssls",
+				"tailwindcss",
+				"emmet_ls",
+				"jsonls",
+				"bashls",
+				"marksman",
+				"jdtls"
 			}
-		})
-	end,
+		}
+	}
 }
