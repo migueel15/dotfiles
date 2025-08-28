7ffed8b20c6ad05a592e83890a008d0aa2c8bd13
migueel15 <miguelangeldorado10@gmail.com>
2025-08-28 14:47:21 +0200

    lspconfig funcional

--- modules/nvim/config/lua/plugins/lspconfig.lua
+++ modules/nvim/config/lua/plugins/lspconfig.lua
@@ -1,6 +1,47 @@
 return {
 	{
 		"neovim/nvim-lspconfig",
+		config = function()
+			-- Configure lua_ls for better vim API detection
+			require('lspconfig').lua_ls.setup({
+				settings = {
+					Lua = {
+						runtime = {
+							version = 'LuaJIT',
+						},
+						diagnostics = {
+							globals = { 'vim' },
+						},
+						workspace = {
+							library = vim.api.nvim_get_runtime_file("", true),
+							checkThirdParty = false,
+						},
+						telemetry = {
+							enable = false,
+						},
+					},
+				},
+			})
+
+			vim.api.nvim_create_autocmd('LspAttach', {
+				callback = function(args)
+					local opts = { buffer = args.buf, noremap = true, silent = true }
+					-- Navigation
+					vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
+					vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+					vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, opts)
+
+					-- Actions
+					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, opts)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
+					vim.keymap.set("i", "<C-k>", vim.lsp.buf.signature_help, opts)
+				end,
+			})
+		end
 	},
 	{
 		"mason-org/mason.nvim",
