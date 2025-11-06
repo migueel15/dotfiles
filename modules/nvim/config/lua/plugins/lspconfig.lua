d720e752e8e821a4a6cce77d5ec3257a06cbfecd
migueel15 <miguelangeldorado10@gmail.com>
2025-11-06 18:41:14 +0100

    discord and nullls nvim

--- modules/nvim/config/lua/plugins/lspconfig.lua
+++ modules/nvim/config/lua/plugins/lspconfig.lua
@@ -69,5 +69,35 @@ return {
 				"astro"
 			}
 		}
+	},
+	{
+		"nvimtools/none-ls.nvim",
+		dependencies = "nvim-lua/plenary.nvim",
+	},
+	{
+		"jay-babu/mason-null-ls.nvim",
+		event = { "BufReadPre", "BufNewFile" },
+		dependencies = {
+			"williamboman/mason.nvim",
+			"nvimtools/none-ls.nvim",
+		},
+		config = function()
+			require("mason").setup()
+			require("mason-null-ls").setup({
+				ensure_installed = {
+					"black"
+				},
+				automatic_installation = true,
+				handlers = {},
+			})
+
+			local null_ls = require("null-ls")
+
+			null_ls.setup({
+				sources = {
+					null_ls.builtins.formatting.black
+				}
+			})
+		end,
 	}
 }
