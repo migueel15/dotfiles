b8b8ad32655db0d5a6ee1ac90c8338a14e7c60f2
migueel15 <miguelangeldorado10@gmail.com>
2026-01-04 05:06:23 +0100

    Update dotfiles: dom 04 ene 2026 05:06:23 CET

--- modules/nvim/config/lua/plugins/nvim-tree.lua
+++ modules/nvim/config/lua/plugins/nvim-tree.lua
@@ -1,103 +1,105 @@
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
-  config = function()
-    require("nvim-tree").setup({
-      filters = {
-        dotfiles = false,
-        exclude = { vim.fn.stdpath("config") .. "/lua/custom" },
-      },
-      disable_netrw = true,
-      hijack_netrw = true,
-      hijack_cursor = true,
-      hijack_unnamed_buffer_when_opening = false,
-      sync_root_with_cwd = true,
-      update_focused_file = {
-        enable = true,
-        update_root = true,
-      },
-      view = {
-        adaptive_size = true,
-        side = "right",
-        width = 30,
-        preserve_window_proportions = true,
-      },
-      git = {
-        enable = true,
-        ignore = false,
-      },
-      filesystem_watchers = {
-        enable = true,
-      },
-      actions = {
-        open_file = {
-          resize_window = true,
-        },
-      },
-      diagnostics = {
-        enable = true,
-      },
-      renderer = {
-        root_folder_label = false,
-        highlight_git = true,
-        highlight_opened_files = "none",
+	"nvim-tree/nvim-tree.lua",
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("nvim-tree").setup({
+			filters = {
+				dotfiles = false,
+				exclude = { vim.fn.stdpath("config") .. "/lua/custom" },
+			},
+			disable_netrw = true,
+			hijack_netrw = true,
+			hijack_cursor = true,
+			hijack_unnamed_buffer_when_opening = false,
+			sync_root_with_cwd = true,
+			update_focused_file = {
+				enable = true,
+				update_root = true,
+			},
+			view = {
+				adaptive_size = true,
+				side = "right",
+				width = 30,
+				preserve_window_proportions = true,
+			},
+			git = {
+				enable = true,
+				ignore = false,
+			},
+			filesystem_watchers = {
+				enable = true,
+			},
+			actions = {
+				open_file = {
+					resize_window = true,
+				},
+			},
+			diagnostics = {
+				enable = true,
+			},
+			renderer = {
+				root_folder_label = false,
+				highlight_git = true,
+				highlight_opened_files = "none",
 
-        indent_markers = {
-          enable = true,
-        },
+				indent_markers = {
+					enable = true,
+				},
 
-        icons = {
-          git_placement = "after",
-          web_devicons = {
-            file = {
-              enable = true,
-              color = true,
-            },
-            folder = {
-              enable = false,
-              color = false,
-            },
-          },
-          show = {
-            file = true,
-            folder = true,
-            folder_arrow = false,
-            git = true,
-          },
+				icons = {
+					git_placement = "after",
+					web_devicons = {
+						file = {
+							enable = true,
+							color = true,
+						},
+						folder = {
+							enable = false,
+							color = false,
+						},
+					},
+					show = {
+						file = true,
+						folder = true,
+						folder_arrow = true,
+						git = true,
+					},
 
-          glyphs = {
-            default = "󰈚",
-            symlink = "",
-            folder = {
-              default = "",
-              empty = "",
-              empty_open = "",
-              open = "",
-              symlink = "",
-              symlink_open = "",
-              arrow_open = "",
-              arrow_closed = "",
-            },
-            git = {
-              unstaged = "✗",
-              staged = "✓",
-              unmerged = "",
-              renamed = "➜",
-              untracked = "★",
-              deleted = "",
-              ignored = "◌",
-            },
-          },
-        },
-      },
-    })
+					glyphs = {
+						default = "󰈚",
+						symlink = "",
+						folder = {
+							default = "󰉋",
+							empty = "󰉖",
+							empty_open = "󰷏",
+							open = "󰝰",
+							symlink = "",
+							symlink_open = "",
+							-- arrow_open = "󰅀",
+							-- arrow_closed = "󰅂",
+							arrow_open = "├",
+							arrow_closed = "│",
+						},
+						git = {
+							unstaged = "✗",
+							staged = "✓",
+							unmerged = "",
+							renamed = "➜",
+							untracked = "★",
+							deleted = "",
+							ignored = "◌",
+						},
+					},
+				},
+			},
+		})
 
-    -- Change HL for git file/folders
-    vim.api.nvim_set_hl(0, "NvimTreeGitFileNewHL", { link = "diffAdded" })
-    vim.api.nvim_set_hl(0, "NvimTreeGitFolderNewHL", { link = "diffAdded" })
-    vim.api.nvim_set_hl(0, "NvimTreeGitNewIcon", { link = "diffAdded" })
+		-- Change HL for git file/folders
+		vim.api.nvim_set_hl(0, "NvimTreeGitFileNewHL", { link = "diffAdded" })
+		vim.api.nvim_set_hl(0, "NvimTreeGitFolderNewHL", { link = "diffAdded" })
+		vim.api.nvim_set_hl(0, "NvimTreeGitNewIcon", { link = "diffAdded" })
 
-    -- Keymaps
-    vim.keymap.set("n", "<leader>t", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle NvimTree" })
-  end,
+		-- Keymaps
+		vim.keymap.set("n", "<leader>t", "<cmd>NvimTreeToggle<cr>", { desc = "Toggle NvimTree" })
+	end,
 }
