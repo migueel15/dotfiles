eb13803305db00e861ec7d457789dd4248bd5b6d
migueel15 <miguelangeldorado10@gmail.com>
2026-01-05 07:27:50 +0100

    Update dotfiles: lun 05 ene 2026 07:27:50 CET

--- modules/nvim/config/lua/plugins/neogen.lua
+++ modules/nvim/config/lua/plugins/neogen.lua
@@ -7,7 +7,7 @@ return {
 		})
 
 		local opts = { noremap = true, silent = true }
-		vim.api.nvim_set_keymap("n", "<Leader>nf", ":lua require('neogen').generate({ type = 'func' })<CR>", opts)
-		vim.api.nvim_set_keymap("n", "<Leader>nc", ":lua require('neogen').generate({ type = 'class' })<CR>", opts)
+		vim.api.nvim_set_keymap("n", "<leader>nf", ":lua require('neogen').generate({ type = 'func' })<CR>", opts)
+		vim.api.nvim_set_keymap("n", "<leader>nc", ":lua require('neogen').generate({ type = 'class' })<CR>", opts)
 	end,
 }
