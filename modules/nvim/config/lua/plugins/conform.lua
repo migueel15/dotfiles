ce676fc38f14f3b3659e9295b876a7c0debb2329
migueel15 <miguelangeldorado10@gmail.com>
2026-04-08 01:26:11 +0200

    set default java formatter

--- modules/nvim/config/lua/plugins/conform.lua
+++ modules/nvim/config/lua/plugins/conform.lua
@@ -10,10 +10,16 @@ return {
 				javascriptreact = { "prettierd", "prettier", stop_after_first = true },
 				typescript = { "prettierd", "prettier", stop_after_first = true },
 				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-				-- java = { "google-java-format" },
+				java = { "google-java-format" },
 			},
 			default_format_opts = {
 				lsp_format = "fallback",
+			},
+
+			formatters = {
+				["google-java-format"] = {
+					prepend_args = { "--aosp", "--skip-sorting-imports", "--skip-removing-unused-imports" },
+				},
 			},
 		})
 	end,
