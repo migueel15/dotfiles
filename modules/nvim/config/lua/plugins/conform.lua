6f27b7dc8a261d9d684de859e574c53d36bc2750
migueel15 <miguelangeldorado10@gmail.com>
2026-04-07 18:14:22 +0200

    Update dotfiles: mar 07 abr 2026 18:14:22 CEST

--- modules/nvim/config/lua/plugins/conform.lua
+++ modules/nvim/config/lua/plugins/conform.lua
@@ -10,6 +10,7 @@ return {
 				javascriptreact = { "prettierd", "prettier", stop_after_first = true },
 				typescript = { "prettierd", "prettier", stop_after_first = true },
 				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+				-- java = { "google-java-format" },
 			},
 			default_format_opts = {
 				lsp_format = "fallback",
