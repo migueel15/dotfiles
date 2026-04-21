1d99163e889d7929f6eed28a24f2f5dd3a27ff2f
migueel15 <miguelangeldorado10@gmail.com>
2026-04-22 01:07:22 +0200

    fix copilot suggestion enabled by default

--- modules/nvim/config/lua/plugins/copilot.lua
+++ modules/nvim/config/lua/plugins/copilot.lua
@@ -4,7 +4,12 @@ return {
 		"AndreM222/copilot-lualine",
 	},
 	config = function()
-		require("copilot").setup({
+		local copilot = require("copilot")
+		local client = require("copilot.client")
+		local command = require("copilot.command")
+		local suggestion = require("copilot.suggestion")
+
+		copilot.setup({
 			suggestion = {
 				auto_trigger = true,
 				keymap = {
@@ -12,17 +17,31 @@ return {
 				},
 			},
 		})
-		vim.keymap.set("n", "<leader>ct", "<cmd>Copilot toggle<cr>", { desc = "Toggle Copilot" })
+
+		-- disabled by default
+		command.disable()
+
+		-- toggle enable/disable
+		--
+
+		vim.keymap.set("n", "<leader>ct", function()
+			if client.is_disabled() then
+				command.enable()
+			else
+				command.disable()
+			end
+		end, { desc = "Toggle Copilot" })
+
 		vim.keymap.set("i", "<Tab>", function()
-			if require("copilot.suggestion").is_visible() then
-				require("copilot.suggestion").accept()
+			if suggestion.is_visible() then
+				suggestion.accept()
 			else
 				return "<Tab>"
 			end
 		end, { expr = true, silent = true })
 		vim.keymap.set("i", "<C-e>", function()
-			if require("copilot.suggestion").is_visible() then
-				require("copilot.suggestion").dismiss()
+			if suggestion.is_visible() then
+				suggestion.dismiss()
 			end
 		end, { silent = true })
 	end,
