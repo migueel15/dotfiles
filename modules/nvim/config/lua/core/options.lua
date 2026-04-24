d271eab0eb994fdf674b29dc66678df2dc211da7
migueel15 <miguelangeldorado10@gmail.com>
2026-04-25 00:57:45 +0200

    Update dotfiles: sáb 25 abr 2026 00:57:45 CEST

--- modules/nvim/config/lua/core/options.lua
+++ modules/nvim/config/lua/core/options.lua
@@ -14,34 +14,34 @@ opt.completeopt = "menuone,noinsert,noselect"
 -- Neovim UI
 -----------------------------------------------------------
 opt.relativenumber = true -- Show line numbers relative to position
-opt.cursorline = true     -- Highlight current line
-opt.number = true         -- Set current line number, if disable 0
-opt.showmatch = true      -- Highlight matching parenthesis
+opt.cursorline = true -- Highlight current line
+opt.number = true -- Set current line number, if disable 0
+opt.showmatch = true -- Highlight matching parenthesis
 opt.foldmethod = "marker" -- Enable folding (default 'foldmarker')
-opt.colorcolumn = "80"    -- Line lenght marker at 80 columns
-opt.splitright = true     -- Vertical split to the right
-opt.splitbelow = true     -- Horizontal split to the bottom
-opt.ignorecase = true     -- Ignore case letters when search
-opt.smartcase = true      -- Ignore lowercase for the whole pattern
-opt.linebreak = true      -- Wrap on word boundary
-opt.laststatus = 3        -- Set global statusline
-opt.swapfile = false      -- Disable swapfiles
+opt.colorcolumn = "80" -- Line lenght marker at 80 columns
+opt.splitright = true -- Vertical split to the right
+opt.splitbelow = true -- Horizontal split to the bottom
+opt.ignorecase = true -- Ignore case letters when search
+opt.smartcase = true -- Ignore lowercase for the whole pattern
+opt.linebreak = true -- Wrap on word boundary
+opt.laststatus = 3 -- Set global statusline
+opt.swapfile = false -- Disable swapfiles
 opt.termguicolors = true
-opt.showmode = false      -- Disable mode info under lualine
+opt.showmode = false -- Disable mode info under lualine
 opt.pumheight = 15
 opt.conceallevel = 1
 
 -----------------------------------------------------------
 -- Tabs, indent
 -----------------------------------------------------------
-opt.expandtab = false  -- Use spaces instead of tabs
-opt.shiftwidth = 4     -- Shift 2 spaces when tab
-opt.tabstop = 4        -- 1 tab == 2 spaces
+opt.expandtab = false -- Use spaces instead of tabs
+opt.shiftwidth = 4 -- Shift 2 spaces when tab
+opt.tabstop = 4 -- 1 tab == 2 spaces
 opt.smartindent = true -- Autoindent new lines
 
 -----------------------------------------------------------
 -- Memory, CPU
 -----------------------------------------------------------
-opt.history = 200      -- Remember N lines in history
+opt.history = 200 -- Remember N lines in history
 opt.lazyredraw = false -- Faster scrolling
-opt.updatetime = 50    -- ms to wait for trigger an event
+opt.updatetime = 50 -- ms to wait for trigger an event
