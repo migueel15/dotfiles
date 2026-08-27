fd4633a162d63f62f9fc7a4b49ee77a2433e602c
migueel15 <miguelangeldorado10@gmail.com>
2026-08-27 20:58:30 +0200

    Update dotfiles: jue 27 ago 2026 20:58:30 CEST

--- modules/hyprland/config/modules/monitors.lua
+++ modules/hyprland/config/modules/monitors.lua
@@ -9,7 +9,7 @@ hl.monitor({
 	output = "desc:BNQ ZOWIE XL LCD N1J03633SL0",
 	mode = "1920x1080@144",
 	position = "2560x0",
-	mirror = config.mirror and "desc:BNQ BenQ EX2710Q TBM00955019" or "",
+	-- mirror = config.mirror and "desc:BNQ BenQ EX2710Q TBM00955019" or "",
 })
 
 hl.monitor({
@@ -17,7 +17,7 @@ hl.monitor({
 	mode = "preferred",
 	position = "auto",
 	scale = "1.67",
-	-- mirror = config.mirror and "desc:BNQ BenQ EX2710Q TBM00955019" or "",
+	mirror = config.mirror and "desc:BNQ BenQ EX2710Q TBM00955019" or "",
 })
 
 hl.monitor({
